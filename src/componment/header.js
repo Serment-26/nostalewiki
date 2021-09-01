187044c250fcec26640bdd294a967be2ccfa24b1
Serment-26 <seanonline12@gmail.com>
2021-09-01 21:12:21 +0800

    nx add prop

--- src/componment/header.js
+++ src/componment/header.js
@@ -5,7 +5,7 @@ export default function Header(){
         <header>
             
             <ul>
-                <li><img src="https://truth.bahamut.com.tw/s01/202105/182f7b66408f6ad87b9c70d06da30316.JPG"/></li>
+                <li><img onClick={()=> window.open('https://forum.gamer.com.tw/C.php?bsn=10995&snA=43735','_blank')} src="https://truth.bahamut.com.tw/s01/202105/182f7b66408f6ad87b9c70d06da30316.JPG" alt="logo"/></li>
                 <li><a herf="#hi">Specialist</a></li>
                 <li><a href="https://docs.google.com/spreadsheets/d/17iUfgd63jeUynL_kllOu5vnlDYZ5Puel/edit?usp=sharing&ouid=113105389545603828192&rtpof=true&sd=true">Tower reward</a></li>
             </ul>
